e0d5dae01894755160815ed28791d91144201668
cat_yu <supercutcat@outlook.com>
2022-11-13 05:49:12 +0800

    chore(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), image()]
-});
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
+})
